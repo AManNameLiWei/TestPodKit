4854c57dc410bc3e031f4597740529bbcdb65c6b
AManNameLiWei <51556966+AManNameLiWei@users.noreply.github.com>
2022-09-13 15:42:04 +0800

    Add files via upload

---
+++ Example/TestPodKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestPodKit
+//
+//  Created by liwei05 on 09/13/2022.
+//  Copyright (c) 2022 liwei05. All rights reserved.
+//
+
+@import UIKit;
+#import "ALLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ALLAppDelegate class]));
+    }
+}
